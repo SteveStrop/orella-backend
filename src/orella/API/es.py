b26700f3bc690ca9ff16144de2519ce3a64ede5f
SteveStrop <steve_tropical@yahoo.co.uk>
2018-09-24 20:33:33 +0100

    Added upload method to es.Grab Added post method (calls es.Grab.upload) to WebGrab ViewAPI

--- src/orella/API/es.py
+++ src/orella/API/es.py
@@ -1,4 +1,5 @@
 import os
+import base64
 
 # location of folders used to store photos
 parent_folder = os.path.join('G:/', 'Estate Agents')
@@ -15,6 +16,11 @@ class Grab:
         """ :return a tuple of folders from the 'parent_folder' with prefixes matching those stored in 'prefix'"""
 
         return [folder for folder in os.listdir(parent_folder) for cond in prefix if cond in folder]
+
+    @staticmethod
+    def upload(file):
+        with open(os.path.join(file['folder'], file['name']), "wb") as photo:
+            print(photo.write(base64.decodebytes(file['file'].encode())))
 
 
 if __name__ == '__main__':
--- src/orella/API/views.py
+++ src/orella/API/views.py
@@ -7,6 +7,11 @@ class WebGrab(APIView):
     """ provides back end support for webGRAB photo import app"""
 
     @staticmethod
-    def get(request, format=None):
+    def get(request):
         """ :return: a tuple of folders from 'parent' directory"""
         return Response(Grab.get_destination_folders())
+
+    @staticmethod
+    def post(request):
+        Grab.upload(request.data)
+        return Response({'message': 'OK'})
